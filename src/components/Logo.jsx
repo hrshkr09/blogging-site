769868f04c307ab8490d8ff94be67b6fc45a3a88
Harsh Kumar <107759338+hrshkr09@users.noreply.github.com>
2023-12-20 17:06:54 +0530

    Add files via upload

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '10px'}) {
+  return (
+    <div>
+      <img src="https://marketplace.canva.com/EAFqjKz2LU8/1/0/1600w/canva-modern-creative-technology-logo-HOLTokMycis.jpg" width = '50px' alt="" />
+    </div>
+  )
+}
+
+export default Logo
